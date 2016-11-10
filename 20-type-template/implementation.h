93c674b9100a679062868a63a8e1887fa9cf15cc
Tomasz Kisielewski <kisielewski.tom@gmail.com>
2016-11-10 11:52:32 +0100

    changes in 22-invalid.cpp

--- 20-type-template/implementation.h
+++ 20-type-template/implementation.h
@@ -1,6 +1,8 @@
 #pragma once
 #include <cstddef>
 #include <utility>
+#include <boost/optional.hpp>
+
 
 template<typename T>
 class container_wrapper{
@@ -26,4 +28,60 @@ private:
 		T _value;
 		
 };
+
+template<>
+class container_wrapper< boost::optional<int> > {
+
+public:	
+		container_wrapper() = default;		
+
+		container_wrapper(boost::optional<int> arg ): _value(std::move( arg )){
+		}
+
+		container_wrapper( const container_wrapper &) = default;
+	
+		container_wrapper( container_wrapper &&) = default;		
+		
+		container_wrapper & operator = (const container_wrapper &) = default;
+
+		container_wrapper &operator = (container_wrapper &&) = default;		
+		
+		std::size_t size() const{
+			return _value ? 1 : 0;;
+		}
+
+private:
+		boost::optional<int> _value;
+		
+};
+
+
+
+template<typename T>
+class container_wrapper< boost::optional<T> >{
+public:	
+		container_wrapper() = default;		
+
+		container_wrapper(T arg): _value(std::move( arg )){
+		}
+
+		container_wrapper( const container_wrapper &) = default;
+	
+		container_wrapper( container_wrapper &&) = default;		
+		
+		container_wrapper & operator = (const container_wrapper &) = default;
+
+		container_wrapper &operator = (container_wrapper &&) = default;		
+		
+		std::size_t size() const{
+			return _value ? 1 : 0;
+		}
+
+private:
+		boost::optional<T> _value;
+		
+};
+
+
+
 
